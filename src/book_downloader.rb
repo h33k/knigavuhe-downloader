5c4d8ea029c035651809f4e9f7a1ffe294444aff
h33k <h33k-dev@proton.me>
2024-08-15 22:17:15 +0500

    mode for separate files

--- src/book_downloader.rb
+++ src/book_downloader.rb
@@ -5,15 +5,31 @@ def main
   get_book
   audio_urls = get_audio_urls
 
-  book_filename = "#{@book_author}. #{@book_title}.mp3"
-  if File.exist?(book_filename)
-    raise "book already exists: #{book_filename}"
+  if audio_urls.size > 1
+    puts 'This book contains several chapters. Choose save mode:'
+    puts "1: Save all chapters as one mp3 file"
+    puts "2: Save each chapter as separate mp3 file"
+    input = gets.chomp.downcase
+    raise "invalid save mode: #{input}" unless %w[1 2].include?(input)
+    @save_mode = input.to_i
+  else
+    @save_mode = 0
   end
+
+  book_dirname = "#{@book_author}. #{@book_title}"
+  book_filename = "#{@book_author}. #{@book_title}.mp3"
+  check_for_existence(book_filename, book_dirname)
+
+  Dir.mkdir(book_dirname) if @save_mode == 2
 
   audio_urls.each_with_index do |audio_url, index|
     puts "[#{index}/#{audio_urls.size}] Download in progress..."
     audio_file = downloader(audio_url)
-    combiner(audio_file, book_filename)
+    save_path = case @save_mode
+                when 2 then "#{book_dirname}/#{index + 1}. #{@book_title}.mp3"
+                else book_filename
+                end
+    @save_mode == 1 ? combiner(audio_file, book_filename) : saver(audio_file, save_path)
   end
 
   puts "[#{audio_urls.size}/#{audio_urls.size}] Book downloaded!"
@@ -48,9 +64,26 @@ def get_audio_urls
   audio_urls
 end
 
+def check_for_existence(filename, dirname)
+  case @save_mode
+  when 0, 1
+    raise "book already exists: #{filename}" if File.exist?(filename)
+  when 2
+    raise "book directory already exists: #{dirname}" if Dir.exist?(dirname)
+  else
+    raise "unknown save mode"
+  end
+end
+
 def downloader(url)
   URI.open(url) do |file|
     return StringIO.new(file.read) # save in memory
+  end
+end
+
+def saver(audio_file, output_filename)
+  File.open(output_filename, 'wb') do |file|
+    file.write(audio_file.read)
   end
 end
 
@@ -69,6 +102,5 @@ def combiner(audio_file, output_filename)
     end
   end
 end
-
 
 main
